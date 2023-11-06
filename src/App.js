8da04d6aebd75fd0e06615bcf26a4dca0e2966b6
hen <whshle08@gmail.com>
2023-11-05 18:02:21 -0800

    spinny spin spin

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import './App.css';
 import Cookie from './assets/cookie.png'
 
-// For this assignment you're going to make the cookie image do ONE 360 spin whenever it is clicked
-
-// This is what you'll need:
-// 1. CSS Animations
-// 2. onClick event handler
-// 3. useRef
-// 4. Make your own function inside App() and classList.add('your-animation') whenever the onClick event is triggered
+import React from 'react';
+import { useRef } from 'react';
 
 function App() {
 
+  let cookie = useRef('cookie');
+
+  let spin = () => {
+    cookie.current.classList.add('spin');
+  }
+
   return (
     <div className="app">
-      <img alt="cookie" className="spin" src={Cookie} draggable="false" />
+      <img alt="cookie" src={Cookie} draggable="false" ref={cookie} onClick={spin}/>
     </div>
   );
+
 }
 
 export default App;
